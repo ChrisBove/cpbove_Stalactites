d36b2e6dddfd5cb9c4f9f1852556219e3da5a27c
Chris Bove <bove.3123@gmail.com>
2016-11-19 11:37:17 -0500

    initial commit for stalactites project

---
+++ src/cpbove/Stalactites.java
@@ -0,0 +1,26 @@
+package cpbove;
+
+import ks.common.games.Solitaire;
+
+public class Stalactites extends Solitaire{
+
+	@Override
+	public String getName() {
+		return "cpbove-Stalactites";
+	}
+
+	@Override
+	public boolean hasWon() {
+		// TODO finish this
+		return false;
+	}
+
+	@Override
+	public void initialize() {
+		// TODO
+		// deck of cards to be dealt
+		
+		
+	}
+
+}
